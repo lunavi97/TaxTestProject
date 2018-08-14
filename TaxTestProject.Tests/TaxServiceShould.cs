22c6de606f0e40c2573b212007e4ca7db87b6dc4
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-14 03:51:25 -0300

    Agregado método de testeo

--- TaxTestProject.Tests/TaxServiceShould.cs
+++ TaxTestProject.Tests/TaxServiceShould.cs
@@ -4,12 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaxTestProject.Services;
 
 namespace TaxTestProject.Tests
 {
     [TestClass]
     public class TaxServiceShould
     {
-
+        [TestMethod]
+        public void Returns1000IfGrossSalaryIs9500()
+        {
+            var taxService = new TaxService();
+            Assert.AreEqual(1000, taxService.GetTax(9500));
+        }
     }
 }

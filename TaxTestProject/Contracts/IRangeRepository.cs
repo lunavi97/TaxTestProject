05f4affa3312162476e0af83ce5f374d9c145502
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-17 02:22:34 -0300

    Modificada carpeta de interfaz por Contracts

---
+++ TaxTestProject/Contracts/IRangeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxTestProject.Entities;
+
+namespace TaxTestProject.Contacts
+{
+    public interface IRangeRepository
+    {
+        Range GetRange(decimal grossSalary);
+    }
+}

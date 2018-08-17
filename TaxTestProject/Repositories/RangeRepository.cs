f434800bc49874ccde65a80bec4464f52b03d907
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-17 01:42:30 -0300

    Desarrollada clase RangeRepository, indicada como implementación de la interfaz e importado Newtonsoft para deserialización de JSON

--- TaxTestProject/Repositories/RangeRepository.cs
+++ TaxTestProject/Repositories/RangeRepository.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaxTestProject.Contacts;
+using TaxTestProject.Entities;
 
 namespace TaxTestProject.Repositories
 {
-    class RangeRepository
+    public class RangeRepository : IRangeRepository
     {
+        private readonly List<Range> _ranges;
+
+        public RangeRepository()
+        {
+            string jsonFile = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, "ranges.json");
+            _ranges = JsonConvert.DeserializeObject<List<Range>>(
+                File.ReadAllText(jsonFile, Encoding.UTF8));
+        }
+
+        public Range GetRange(decimal grossSalary)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
